1f6f0c9889b133cf4ca87435a4983bd930f71344
ken <ken.mbira@student.moringaschool.com>
2022-04-20 07:36:45 +0300

    set color changing functionality

--- app/src/main/java/devmbira/mobile/kidsdrawingapp/MainActivity.kt
+++ app/src/main/java/devmbira/mobile/kidsdrawingapp/MainActivity.kt
@@ -5,8 +5,10 @@ import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.ImageButton
 import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.core.view.get
 import com.google.android.material.slider.RangeSlider
@@ -84,5 +86,21 @@ class MainActivity : AppCompatActivity() {
             mBrushSizeSliderValue = value
         }
         brushDialog.show()
+    }
+
+    fun paintClicked(view: View){
+        if(view !== mImageButtonCurrentPaint){
+            val imageButton = view as ImageButton
+            val colorTag = imageButton.tag.toString()
+            drawingView?.setColor(colorTag)
+            imageButton.setImageDrawable(
+                ContextCompat.getDrawable(this,R.drawable.pallete_pressed)
+            )
+            mImageButtonCurrentPaint!!.setImageDrawable(
+                ContextCompat.getDrawable(this,R.drawable.pallete_normal)
+            )
+
+            mImageButtonCurrentPaint = view
+        }
     }
 }
